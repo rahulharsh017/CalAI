3a8eb96aead840ce2a3e1ddde575404135497a59
Rahul Harsh <117423855+rahulharsh017@users.noreply.github.com>
2024-01-05 18:39:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// gsap.to("#nav",{
+//     backgroundColor:"#000",
+//     duration:0.5,
+//     height:"140px",
+//     scrollTrigger:{
+//         trigger:"#nav",
+//         scroller:"body",
+//         // markers:true,
+//         start:"top -10%",
+//         end:"top -11%",
+//         scrub:1
+//     }
+// })
 
 
 // gsap.to("#main",{
