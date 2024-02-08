1b5c5aacee16a50071c75651eee1f00c3bcb30cb
Rahul Harsh <117423855+rahulharsh017@users.noreply.github.com>
2024-02-09 01:26:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -33,6 +33,41 @@ navmenuToggler.addEventListener("click",() => {
       answer.style.display = 'block';
       }
   }
+
+  
+    const certificate = document.getElementById('certificate');
+    const dropdown = document.querySelector('.dropdown');
+     let hoverTimeout; // Variable to store the timeout ID
+
+ 
+    // Handle mouse hover on the Certifications link
+    certificate.addEventListener('mouseover', function() {
+      clearTimeout(hoverTimeout); // Clear any existing timeout
+      dropdown.style.display = 'block';
+    });
+
+    // Handle mouse leave from the Certifications link with a delay
+    certificate.addEventListener('mouseleave', function() {
+     hoverTimeout = setTimeout(function() {
+        dropdown.style.display = 'none';
+      }, 500); // Adjust the delay duration (in milliseconds) as needed
+    });
+
+    // Handle clicks on dropdown links
+    const dropdownLinks = document.querySelectorAll('#certificate .dropdown a');
+    dropdownLinks.forEach(function(link) {
+      link.addEventListener('click', function() {
+        console.log('Clicked on:', link.textContent);
+        // Add your click event handling logic here
+        dropdown.style.display = 'none'; // Close the dropdown after a link is clicked
+      });
+    });
+
+
+
+
+
+
    
 
 
